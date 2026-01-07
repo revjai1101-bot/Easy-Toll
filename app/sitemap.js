02bbcd0791cedb08c5d87f56d8f67a0588905d4e
Revin Ramuh <revjai1101@gmail.com>
2026-01-07 11:26:16 +0800

    Added Privacy page and updated sitemap

--- app/sitemap.js
+++ app/sitemap.js
@@ -20,5 +20,11 @@ export default function sitemap() {
       changeFrequency: 'monthly',
       priority: 0.8,
     },
+    {
+      url: `${baseUrl}/privacy`,
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
   ];
 }
